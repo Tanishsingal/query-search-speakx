72e3a736d10c1000bb6bd3cb6aca89ab75870de3
Tanish singal <tanishsingal245@gmail.com>
2025-01-26 22:30:40 +0530

    frontend added

---
+++ query-search-f/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import QuestSearch from './components/QuestionSearch'
+
+function App() {
+
+  return (
+    <>
+      <QuestSearch/>
+    </>
+  )
+}
+
+export default App
